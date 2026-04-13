b16dfc0e323c3c2a5909322dc528c02acb8c2502
Konstiantin Kiselev <konstantin.kiselev.1998@mail.ru>
2026-04-13 13:44:00 +0500

    Update test_use_case_update_user_propagates_error added  assert_called_once_with checks

--- backend/tests/unit/use_cases/user/test_update.py
+++ backend/tests/unit/use_cases/user/test_update.py
@@ -26,12 +26,17 @@ def test_use_case_update_user_calls_service(user_service_mock, user_entity):
 
 def test_use_case_update_user_propagates_error(user_service_mock):
     user_data = UserUpdateDTO(username='new_username')
+    user_id = 1
 
     user_service_mock.update_user.side_effect = UserNameAlreadyExistsError
-
     with pytest.raises(UserNameAlreadyExistsError):
         UpdateUser(
             service=user_service_mock,
-            user_id=1,
+            user_id=user_id,
             user_data=user_data,
         )()
+
+    user_service_mock.update_user.assert_called_once_with(
+        user_id=user_id,
+        user_data=user_data,
+    )
